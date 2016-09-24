4d6431906d6d9c2ffc68c2a6a530e78422a2d1cf
Kirill Berezin <enelar@exsul.net>
2016-09-24 21:09:48 +0300

    Update coding style

--- float-youtube@exsul/extension.js
+++ float-youtube@exsul/extension.js
@@ -10,20 +10,29 @@ const St = imports.gi.St;
 const Main = imports.ui.main;
 
 
-function id(x) { return x }
-
-function plus1(n) { return n + 1 }
-
-
-function init(em) {
-    return new Preview(em)
+function id(x)
+{
+  return x;
 }
 
-function Preview(em) {
-    this.init(em)
+function plus1(n)
+{
+  return n + 1;
 }
 
-Preview.prototype = {
+
+function init(em)
+{
+  return new Preview(em)
+}
+
+function Preview(em)
+{
+  this.init(em)
+}
+
+Preview.prototype =
+{
     preview: null,
     workspaceSwitchSignal: null,
     overviewShowingSignal: null,
@@ -33,141 +42,157 @@ Preview.prototype = {
     corner: 0,
     overview: false,
 
-    init: function(em) {
-        this.extensionMeta = em
+    init: function(em)
+    {
+      this.extensionMeta = em
     },
 
-    enable: function() {
-        this.workspaceSwitchSignal =
-            global.screen.connect( "workspace-switched" , Lang.bind(this, this.mpvFloat)
+    enable: function()
+    {
+      this.workspaceSwitchSignal
+        = global.screen.connect( "workspace-switched" , Lang.bind(this, this.mpvFloat)
                                  )
-        this.overviewHidingSignal =
-            Main.overview.connect( "hiding"
+      this.overviewHidingSignal
+        =  Main.overview.connect( "hiding"
                                  , Lang.bind(this, this.toggleView, false)
                                  )
-        this.overviewShowingSignal =
-            Main.overview.connect( "showing"
+      this.overviewShowingSignal
+        = Main.overview.connect( "showing"
                                  , Lang.bind(this, this.toggleView, true)
                                  )
     },
 
-    disable: function() {
+    disable: function()
+    {
+      this.removePreview()
+      global.screen.disconnect(this.workspaceSwitchSignal)
+      Main.overview.disconnect(this.overviewHidingSignal)
+      Main.overview.disconnect(this.overviewShowingSignal)
+    },
+
+    toggleView: function(_, active)
+    {
+      this.overview = active
+      if (active)
         this.removePreview()
-        global.screen.disconnect(this.workspaceSwitchSignal)
-        Main.overview.disconnect(this.overviewHidingSignal)
-        Main.overview.disconnect(this.overviewShowingSignal)
+      else
+        this.mpvFloat()
     },
 
-    toggleView: function(_, active) {
-        this.overview = active
-        if (active) {
-            this.removePreview()
-
-        } else {
-            this.mpvFloat()
-        }
+    mpvFloat: function()
+    {
+      let mpv = this.getMPVWindow()
+      if (mpv !== null && ! this.overview)
+        this.showPreview(mpv)
+      else
+        this.removePreview()
     },
 
-    mpvFloat: function() {
-        let mpv = this.getMPVWindow()
-        if (mpv !== null && ! this.overview) {
-            this.showPreview(mpv)
+    getMPVWindow: function()
+    {
+      let mpv = null
+      let len = global.screen.n_workspaces
 
-        } else {
-            this.removePreview()
+      for (let i = 0; i < len; i++)
+      {
+        if (global.screen.get_active_workspace_index() === i)
+          continue;
+
+        let ws = global.screen.get_workspace_by_index(i)
+        let wins = ws.list_windows()
+
+        for (let j = 0, l = wins.length; j < l; j++) {
+          if (wins[j].get_title().search("YouTube") > -1 )
+          {
+            mpv = wins[j]
+            break;
+          }
         }
-    },
+      }
 
-    getMPVWindow: function() {
-        let mpv = null
-        let len = global.screen.n_workspaces
-
-        for (let i = 0; i < len; i++) {
-            if (global.screen.get_active_workspace_index() === i) continue
-
-            let ws = global.screen.get_workspace_by_index(i)
-            let wins = ws.list_windows()
-
-            for (let j = 0, l = wins.length; j < l; j++) {
-                if (wins[j].get_title().search("YouTube") > -1 ) {
-                    mpv = wins[j]
-                    break;
-                }
-            }
-        }
-
-        return mpv
+      return mpv;
     },
 
     // | Takes a function that modifies the corner int
     // switchCorner :: (Int -> Int) -> IO ()
-    switchCorner: function(f) {
-        let g = Main.layoutManager.getWorkAreaForMonitor(0)
+    switchCorner: function(f)
+    {
+      let g = Main.layoutManager.getWorkAreaForMonitor(0)
 
-        this.corner = f(this.corner) % 4
-        global.log("corner: " + this.corner)
-        switch(this.corner) {
-            case 0:
-                this.posX = g.x + 10
-                this.posY = g.y + 10
-                break
+      this.corner = f(this.corner) % 4
+      global.log("corner: " + this.corner)
+      switch(this.corner)
+      {
+        case 0:
+          this.posX = g.x + 10
+          this.posY = g.y + 10
+        break
 
-            case 1:
-                this.posX = g.x + g.width - this.preview.get_width() - 10
-                this.posY = g.y + 10
-                break
+        case 1:
+          this.posX = g.x + g.width - this.preview.get_width() - 10
+          this.posY = g.y + 10
+        break
 
-            case 2:
-                this.posX = g.x + g.width - this.preview.get_width() - 10
-                this.posY = g.y + g.height - this.preview.get_height() - 10
-                break
+        case 2:
+          this.posX = g.x + g.width - this.preview.get_width() - 10
+          this.posY = g.y + g.height - this.preview.get_height() - 10
+          break
 
-            case 3:
-                this.posX = g.x + 10
-                this.posY = g.y + g.height - this.preview.get_height() - 10
-        }
+        case 3:
+          this.posX = g.x + 10
+          this.posY = g.y + g.height - this.preview.get_height() - 10
+          break
+      }
 
-        this.preview.set_position(this.posX, this.posY)
+      this.preview.set_position(this.posX, this.posY)
     },
 
-    showPreview: function(win) {
-        this.removePreview()
+    showPreview: function(win)
+    {
+      this.removePreview()
 
-        this.preview = new St.Button({ style_class: "youtube-preview" })
-        let th = this.getThumbnail(win, 320)
+      this.preview = new St.Button({ style_class: "youtube-preview" })
+      let th = this.getThumbnail(win, 100)
 
-        this.preview.connect( "enter-event"
+      this.preview.connect( "enter-event"
                             , Lang.bind(this, _ => this.switchCorner(plus1))
                             )
 
-        this.preview.add_actor(th)
-        this.switchCorner(id)
+      this.preview.add_actor(th)
+      this.switchCorner(id)
 
-        Main.layoutManager.addChrome(this.preview)
+      Main.layoutManager.addChrome(this.preview)
     },
 
-    removePreview: function() {
-        if (this.preview !== null) {
-            this.preview.destroy()
-            this.preview = null
-        }
+    removePreview: function()
+    {
+      if (this.preview !== null)
+      {
+        this.preview.destroy()
+        this.preview = null
+      }
     },
 
-    getThumbnail: function(win, size) {
-        let th = null
-        let mutw = win.get_compositor_private()
+    getThumbnail: function(win, size)
+    {
+      let th = null
+      let mutw = win.get_compositor_private()
 
-        if (mutw) {
-            let wtext = mutw.get_texture()
-            let [width, height] = wtext.get_size()
-            let scale = Math.min(1.0, size / width, size / height)
-            th = new Clutter.Clone({ source: wtext
-                                   , reactive: true
-                                   , width: width * scale
-                                   , height: height * scale
-                                   })
-        }
+      if (mutw)
+      {
+        let wtext = mutw.get_texture()
+        let [width, height] = wtext.get_size()
+        let scale = Math.min(1.0, size / width, size / height)
+        th = new Clutter.Clone
+        ({
+           source: wtext
+           , reactive: true
+           , width: width * scale
+           , height: height * scale
+        });
 
-        return th
+      }
+
+      return th
     }
 }
