a485e82db05503a7b61278417837764bff636208
Kirill Berezin <enelar@exsul.net>
2016-09-25 00:27:33 +0300

    Move everything left

--- float-youtube@exsul/extension.js
+++ float-youtube@exsul/extension.js
@@ -33,121 +33,111 @@ function Preview(em)
 
 Preview.prototype =
 {
-    //preview: null,
-    workspaceSwitchSignal: null,
-    overviewShowingSignal: null,
-    overviewHidingSignal: null,
-    //posX: 0,
-    //posY: 0,
-    //corner: 0,
-    //overview: false,
+  init: function(em)
+  {
+    this.extensionMeta = em
+  },
 
-    init: function(em)
-    {
-      this.preview = undefined;
-      this.extensionMeta = em
-    },
+  enable: function()
+  {
+    this.workspaceSwitchSignal
+      = global.screen.connect( "workspace-switched" , Lang.bind(this, this.mpvFloat)
+                               )
+    this.overviewHidingSignal
+      =  Main.overview.connect( "hiding"
+                               , Lang.bind(this, this.toggleView, false)
+                               )
+    this.overviewShowingSignal
+      = Main.overview.connect( "showing"
+                               , Lang.bind(this, this.toggleView, true)
+                               )
+  },
 
-    enable: function()
-    {
-      this.workspaceSwitchSignal
-        = global.screen.connect( "workspace-switched" , Lang.bind(this, this.mpvFloat)
-                                 )
-      this.overviewHidingSignal
-        =  Main.overview.connect( "hiding"
-                                 , Lang.bind(this, this.toggleView, false)
-                                 )
-      this.overviewShowingSignal
-        = Main.overview.connect( "showing"
-                                 , Lang.bind(this, this.toggleView, true)
-                                 )
-    },
+  disable: function()
+  {
+    this.despawn_window()
+    global.screen.disconnect(this.workspaceSwitchSignal)
+    Main.overview.disconnect(this.overviewHidingSignal)
+    Main.overview.disconnect(this.overviewShowingSignal)
+  },
 
-    disable: function()
-    {
+  toggleView: function(_, active)
+  {
+    this.overview = active
+    if (active)
       this.despawn_window()
-      global.screen.disconnect(this.workspaceSwitchSignal)
-      Main.overview.disconnect(this.overviewHidingSignal)
-      Main.overview.disconnect(this.overviewShowingSignal)
-    },
+    else
+      this.mpvFloat()
+  },
 
-    toggleView: function(_, active)
-    {
-      this.overview = active
-      if (active)
-        this.despawn_window()
-      else
-        this.mpvFloat()
-    },
-
-    mpvFloat: function()
-    {
-      let mpv = this.find_window("YouTube")
-      if (mpv !== null && ! this.overview)
-        this.spawn_window(mpv)
-      else
-        this.despawn_window()
-    },
-
-    find_window: function(title)
-    {
-      let active_workspace_index
-        = global.screen.get_active_workspace_index();
-
-      let active_workspace
-        = global.screen.get_workspace_by_index(active_workspace_index);
-
-      let windows
-        = active_workspace.list_windows();
-
-      for (let i in windows)
-        if (windows[i].get_title().search(title) > -1)
-          return windows[i];
-
-      return null;
-    }
-    ,
-
-
-    spawn_window: function(win)
-    {
+  mpvFloat: function()
+  {
+    let mpv = this.find_window("YouTube")
+    if (mpv !== null && ! this.overview)
+      this.spawn_window(mpv)
+    else
       this.despawn_window()
+  },
 
-      this.preview = new St.Button({ style_class: "youtube-preview" })
+  find_window: function(title)
+  {
+    let active_workspace_index
+      = global.screen.get_active_workspace_index();
 
-      let th = this.generate_texture(win, 100)
-      this.preview.add_actor(th)
+    let active_workspace
+      = global.screen.get_workspace_by_index(active_workspace_index);
 
-      Main.layoutManager.addChrome(this.preview)
-    },
+    let windows
+      = active_workspace.list_windows();
 
-    despawn_window: function()
-    {
-      if (!this.preview)
-        return;
+    for (let i in windows)
+      if (windows[i].get_title().search(title) > -1)
+        return windows[i];
 
-      this.preview.destroy()
-      this.preview = null
-    },
+    return null;
+  }
+  ,
 
-    generate_texture: function(win, size)
-    {
-      let mutw = win.get_compositor_private()
 
-      if (!mutw)
-        return;
+  spawn_window: function(win)
+  {
+    this.despawn_window()
 
-      let wtext = mutw.get_texture()
-      let [width, height] = wtext.get_size()
-      let scale = Math.min(1.0, size / width, size / height)
-      let th = new Clutter.Clone
-      ({
-         source: wtext
-         , reactive: true
-         , width: width * scale
-         , height: height * scale
-      });
+    this.preview = new St.Button({ style_class: "youtube-preview" })
 
-      return th
-    }
+    let th = this.generate_texture(win, 100)
+    this.preview.add_actor(th)
+
+    Main.layoutManager.addChrome(this.preview)
+  },
+
+  despawn_window: function()
+  {
+    if (!this.preview)
+      return;
+
+    this.preview.destroy()
+    this.preview = null
+  },
+
+  generate_texture: function(win, size)
+  {
+    let mutw = win.get_compositor_private()
+
+    if (!mutw)
+      return;
+
+    let wtext = mutw.get_texture()
+    let [width, height] = wtext.get_size()
+    let scale = Math.min(1.0, size / width, size / height)
+    let th = new Clutter.Clone
+    ({
+       source: wtext
+       , reactive: true
+       , width: width * scale
+       , height: height * scale
+    });
+
+    return th
+  }
 }
